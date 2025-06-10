7ca5c1cbe3a60ecca1c87ac1047be9f4e635848b
Nathan <nathanstasin@zohomail.eu>
2025-06-10 07:52:33 +0200

    begin projects

--- mobile/composeApp/src/commonMain/kotlin/io/asterixorobelix/afrikaburn/ui/directions/DirectionsScreen.kt
+++ mobile/composeApp/src/commonMain/kotlin/io/asterixorobelix/afrikaburn/ui/directions/DirectionsScreen.kt
@@ -2,20 +2,25 @@ package io.asterixorobelix.afrikaburn.ui.directions
 
 import afrikaburn.composeapp.generated.resources.New_Turnoff
 import afrikaburn.composeapp.generated.resources.Res
+import afrikaburn.composeapp.generated.resources.about_title
 import afrikaburn.composeapp.generated.resources.button_directions
 import afrikaburn.composeapp.generated.resources.cd_about_page4_image
 import afrikaburn.composeapp.generated.resources.direction_content
+import afrikaburn.composeapp.generated.resources.direction_sub_title
 import afrikaburn.composeapp.generated.resources.direction_title
 import afrikaburn.composeapp.generated.resources.direction_url
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import io.asterixorobelix.afrikaburn.Dimens
 import io.asterixorobelix.afrikaburn.ui.about.AboutPageContent
 import org.jetbrains.compose.resources.painterResource
@@ -31,8 +36,15 @@ fun DirectionsScreen() {
             .verticalScroll(rememberScrollState())
             .padding(Dimens.paddingMedium)
     ) {
+        Text(
+            text = stringResource(Res.string.direction_title),
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(Dimens.paddingMedium)
+        )
         AboutPageContent(
-            title = stringResource(Res.string.direction_title),
+            title = stringResource(Res.string.direction_sub_title),
             content = stringResource(Res.string.direction_content),
             buttonText = stringResource(Res.string.button_directions),
             url = stringResource(Res.string.direction_url),
