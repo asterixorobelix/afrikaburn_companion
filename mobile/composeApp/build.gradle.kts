b30e21b507669143850988b188cdd7a3c2555628
Nathan <nathanstasin@zohomail.eu>
2025-06-10 16:51:22 +0200

    bump version and add offline map implementation plan

--- mobile/composeApp/build.gradle.kts
+++ mobile/composeApp/build.gradle.kts
@@ -97,8 +97,8 @@ android {
         applicationId = "io.asterixorobelix.afrikaburn"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "2.1"
     }
     packaging {
         resources {
