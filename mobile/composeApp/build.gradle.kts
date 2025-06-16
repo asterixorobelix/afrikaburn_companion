6232fd19e4c9627b0360b2a75ceb618504729e2b
GitHub Action <action@github.com>
2025-06-16 09:13:36 +0000

    Bump version to v2.1.7

--- mobile/composeApp/build.gradle.kts
+++ mobile/composeApp/build.gradle.kts
@@ -101,8 +101,8 @@ android {
         applicationId = "io.asterixorobelix.afrikaburn"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 9
-        versionName = "2.1.6"
+        versionCode = 10
+        versionName = "2.1.7"
     }
     packaging {
         resources {
