c8c66d713e62424818530d0da937afa9e3dab726
GitHub Action <action@github.com>
2026-01-20 15:36:28 +0000

    Bump version to v2.1.21

--- mobile/composeApp/build.gradle.kts
+++ mobile/composeApp/build.gradle.kts
@@ -125,8 +125,8 @@ android {
         applicationId = "io.asterixorobelix.afrikaburn"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 23
-        versionName = "2.1.20"
+        versionCode = 24
+        versionName = "2.1.21"
     }
     packaging {
         resources {
