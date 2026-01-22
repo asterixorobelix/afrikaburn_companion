3f6a5c3ae61d7a0e30eab43e050a0671f37308a8
Nathan <nathanstasin@zohomail.eu>
2026-01-22 12:20:12 +0200

    refactor(05-01): register EventDateService in DI
    REFACTOR phase - Koin dependency injection integration:
    - Added Clock interface registration (DefaultClock)
    - Added EventDateService registration (EventDateServiceImpl)
    - All tests pass (12 EventDateService tests + full suite)
    - No detekt violations in new code
    
    EventDateService now injectable via Koin: get<EventDateService>()

--- mobile/composeApp/src/commonMain/kotlin/io/asterixorobelix/afrikaburn/di/DomainModule.kt
+++ mobile/composeApp/src/commonMain/kotlin/io/asterixorobelix/afrikaburn/di/DomainModule.kt
@@ -2,8 +2,16 @@ package io.asterixorobelix.afrikaburn.di
 
 import io.asterixorobelix.afrikaburn.data.repository.ProjectsRepositoryImpl
 import io.asterixorobelix.afrikaburn.domain.repository.ProjectsRepository
+import io.asterixorobelix.afrikaburn.domain.service.DefaultClock
+import io.asterixorobelix.afrikaburn.domain.service.EventDateService
+import io.asterixorobelix.afrikaburn.domain.service.EventDateServiceImpl
+import kotlinx.datetime.Clock
 import org.koin.dsl.module
 
 val domainModule = module {
     single<ProjectsRepository> { ProjectsRepositoryImpl(get()) }
+
+    // Event date detection for Surprise Mode
+    single<Clock> { DefaultClock() }
+    single<EventDateService> { EventDateServiceImpl(get()) }
 }
